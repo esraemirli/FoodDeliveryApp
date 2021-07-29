d38b8932321acd1025d79026778cc4d60bbd9c03
iiremdogan <doganirem96@gmail.com>
2021-07-29 18:22:35 +0300

    Yemegin icindekileri ekleme bolumu tamamlanmadi

---
+++ app/src/main/java/com/example/fooddeliveryapp/ui/restaurantadd/RestaurantAddFragment.kt
@@ -0,0 +1,99 @@
+package com.example.fooddeliveryapp.ui.restaurantadd
+
+import android.app.Activity
+import android.app.TimePickerDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.fooddeliveryapp.R
+import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RestaurantAddFragment : Fragment(){
+
+    private lateinit var addRestaurantImageView : ImageView
+    private lateinit var openTxt : TextInputEditText
+    private lateinit var closeTxt : TextInputEditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_restaurant_add, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addRestaurantImageView = view.findViewById(R.id.addRestaurantLogo)
+        openTxt = view.findViewById(R.id.restaurantOpenHourEditText)
+        closeTxt = view.findViewById(R.id.restaurantCloseHourEditText)
+
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+            setHomeAsUpIndicator(R.drawable.ic_back)
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    fun addRestaurantLogo(view: View) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startForResult.launch(intent)
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val selectedImage : Uri = result.data?.data!!
+            // TODO : Picasso kütüphanesi eklenecek
+            //Picasso.get().load(selectedImage).fit().centerCrop().into(img)
+        }
+    }
+
+    /*
+    //TODO : Boşluğa tıklandığında klavyeyi saklamak için
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(this.currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+    */
+
+    fun setRestaurantOpenHour(view: View) {
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            openTxt.setText(SimpleDateFormat("HH:mm", Locale.US).format(cal.time))
+        }
+        TimePickerDialog(activity, R.style.DialogTheme, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        //TimePickerDialog(this, R.style.DialogTheme, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+    }
+
+    fun setRestaurantCloseHour(view: View) {
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            closeTxt.setText(SimpleDateFormat("HH:mm", Locale.US).format(cal.time))
+        }
+        TimePickerDialog(activity, R.style.DialogTheme, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        //TimePickerDialog(this, R.style.DialogTheme, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+    }
+
+}
